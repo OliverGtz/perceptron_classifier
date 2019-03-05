1d448bbbbc2d085f261fe9d7ad8daca6923b7d54
olivergutierrez <gutio309@coyote.csusb.edu>
2019-03-05 00:13:12 -0800

    Version 1.0 submitted project in time for 1st deadline

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@
 // Final project: Option 1
 // Binary Classification of Iris Plants Using Single-Layer Perceptron Algorithm
 
+
+// Training Data in data.js
+// Test Data in data.js
+
+
 // Cache HTML elements:
 let trainModelBtn = document.getElementById("train_model_btn");
 let testDataBtn = document.getElementById("test_data_btn");
 let trainedModelP = document.getElementById("trained_model_p");
 
 // Event Listeners:
+
 trainModelBtn.addEventListener('click', function(){
 	console.clear();
 	// Train with training data D[];
-	let trainedModel = trainModel(D, 10);
+	let trainedModel = trainModel(D, 2);
 
 	let vectorString = "";
 	for (let i = 0; i < trainedModel.length; i++){
@@ -38,8 +44,6 @@ testDataBtn.addEventListener('click', function(){
 	classify(E, inputWeightVector);
 });
 
-// Training Data in data.js
-// Test Data in data.js
 
 
 // Initial Weight vector corresponds to every feature
@@ -50,17 +54,12 @@ const W = [0,   0,  0,  0,  0];
 let breakEarly = false;
 let epochs = 5;
 
-
-
-
-
 // Saved models from previous data
 //Model from training iris-modified.setAB data (full set):
-W2 = [0.2,-0.06666666680000002,0.2416666666,-0.23414634120000002,-0.22352941200000004];
+// W2 = [0.2,-0.06666666680000002,0.2416666666,-0.23414634120000002,-0.22352941200000004];
 
 //Model from training iris-modified-setAB.csv data ( about 60 instances);
-W3 = [0.2,-0.15555555580000002,0.1499999998,-0.2585365854,-0.27058823540000004];
-
+// W3 = [0.2,-0.15555555580000002,0.1499999998,-0.2585365854,-0.27058823540000004];
 
 // Now Classify with new data.
 // Test data , E[], brought it from /js/data.js
