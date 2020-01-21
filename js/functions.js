f19f4911091d9a5f37c706707c7cead523ec4031
OliverGtz <olig.333@gmail.com>
2020-01-20 17:39:44 -0800

    Refactored some code and converted to ES Modules

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,7 @@
 // ============= output(instance_, weightVector_) =========
 // ========================================================
 // Linear combination function also known as sign function.
-// It returns a predicted output for a single instance in
-// the dataset. 
+// It returns a predicted output for a single instance in the dataset. 
 function output(instance_, weightVector_){
 	let linearSum = 0;
 
@@ -21,25 +20,22 @@ function output(instance_, weightVector_){
 }
 
 
-
 // ========================================================
 // ========= updateWeightVector(instance_, error) =========
 // ========================================================
 // For a single instance_, update all weights based on formula:
 // wi = wi + learningRate * (target_output - expected_output) * xi
-function updateWeightVector(instance_, error){
+function updateWeightVector(instance_, error, weightVector){
 	let trainingRate = 0.1;
-	console.log("inside updateWeightVector, with W= " + W);
+	console.log("inside updateWeightVector, with W= " + weightVector);
 
 	// update for every weight in W
-	for (let j = 0; j < W.length; j++ ){
+	for (let j = 0; j < weightVector.length; j++ ){
 
-		W[j] += trainingRate*error*instance_[j];
-		console.log("newWeight for w" + j + " is " + W[j]);
+		weightVector[j] += trainingRate*error*instance_[j];
+		console.log("newWeight for w" + j + " is " + weightVector[j]);
 	}
 }
-
-
 
 
 // ========================================================
@@ -48,7 +44,8 @@ function updateWeightVector(instance_, error){
 // Learning is updating the weight vectors so that our expected
 // predictions match our target output. trainModel(D) accepts
 // a dataset array with n instances each with k features.
-function trainModel(D, iterations_){
+function trainModel(D, iterations_, weightVector){
+	let breakEarly = false;
 	let epochs = iterations_;
 	let labelPos = D[0].length -1;			// Class Label is last column on all instances
 
@@ -57,7 +54,7 @@ function trainModel(D, iterations_){
 
 		// If the model predicts the output with 100% accuracy,
 		// then break early of iterations
-		if ( breakEarly){
+		if ( breakEarly ){
 			console.log("+++++++++++++++ FINISHED EARLY ++++++++++++++");
 			break;	
 		}
@@ -72,12 +69,12 @@ function trainModel(D, iterations_){
 			// console.log("Expected Output for instance#" + (h+1) + " is " + output(D[h], W));
 		}
 
-		breakEarly = true;
+		// breakEarly = true;
 
 		// Run perceptron on every instance (Test Data)
 		for (let i = 0; i < D.length; i++ ){
 			// console.log("\nRunning on instance #" + i);
-			let expectedOutput	= output(D[i], W);
+			let expectedOutput	= output(D[i], weightVector);
 			let target					= D[i][labelPos];
 			let error 					= target - expectedOutput;
 
@@ -86,7 +83,7 @@ function trainModel(D, iterations_){
 			// if there's an error, update the weight vectors
 			// Note: error could be negative, so check for error == 0.
 			if( error !== 0){
-				updateWeightVector(D[i], error);
+				updateWeightVector(D[i], error, weightVector);
 				breakEarly = false;
 			} 
 
@@ -96,28 +93,26 @@ function trainModel(D, iterations_){
       // console.log("Error after update is " + error);
 		}	
 
-		console.log("\nFinal weight vector: " + W);
+		console.log("\nFinal weight vector: " + weightVector);
 		console.log("Final training set: ");
 		console.dir(D);
 	}
-	return W;
+	return weightVector;
 }
 
 
-
 // ========================================================
-// =========== classify(datasete_, weightVector_) =========
+// =========== classify(dataset_, weightVector_) =========
 // ========================================================
 function classify(dataset_, weightVector_){
-	let predictions = [];
-	let totalInstances = E.length;
+	console.log('Classifying dataset...');
+	let totalInstances = dataset_.length;
 	let truePositives = 0;
 	let expectedOutput = null;
 	for( let i = 0; i < totalInstances; i++){
 		expectedOutput = output(dataset_[i], weightVector_);
-		predictions.push(expectedOutput);
 
-		console.log("Iteration #" + i + ": Target vs Expected: " + dataset_[i][5] + " : " + expectedOutput);
+		console.log(`Instance #${i}: Target vs Expected: ${dataset_[i][5]} : ${expectedOutput}`);
 		
 		if ( expectedOutput === dataset_[i][5]){
 			truePositives++;
@@ -125,10 +120,12 @@ function classify(dataset_, weightVector_){
 			console.error("False Positive");
 		}
 	}
-	correctlyClassifiedRate = (truePositives / totalInstances) * 100;
+	const correctlyClassifiedRate = (truePositives / totalInstances) * 100;
 
-	console.log("Classified with model: [ " + weightVector_.toString() + " ]");
-	console.log("True Positives: " + truePositives);
-	console.log("Total Instances: " + totalInstances);
-	console.log("Accuracy: " + correctlyClassifiedRate + "%");
+	console.log(`Classified with model: [ ${weightVector_.toString()} ]`);
+	console.log(`True Positives: ${truePositives}`);
+	console.log(`Total Instances: ${totalInstances}`);
+	console.log(`Accuracy: ${correctlyClassifiedRate}%`);
 }
+
+export { classify, trainModel };
--- js/main.js
+++ js/main.js
@@ -4,64 +4,39 @@
 // Binary Classification of Iris Plants Using Single-Layer Perceptron Algorithm
 
 
-// Training Data in data.js
-// Test Data in data.js
-
+// import test and training data from data.js
+import { E as testData, D as trainingData} from "./data.js";
+import { classify, trainModel } from "./functions.js";
 
 // Cache HTML elements:
 let trainModelBtn = document.getElementById("train_model_btn");
 let testDataBtn = document.getElementById("test_data_btn");
 let trainedModelP = document.getElementById("trained_model_p");
 
-// Event Listeners:
-
-trainModelBtn.addEventListener('click', function(){
-	console.clear();
-	// Train with training data D[];
-	let trainedModel = trainModel(D, 2);
-
-	let vectorString = "";
-	for (let i = 0; i < trainedModel.length; i++){
-		vectorString += trainedModel[i];
-		if (i !== trainedModel.length-1) vectorString += ", ";
-	}
-
-	trainedModelP.innerHTML = "W2 = [ " + vectorString + " ]";
-});
-
-testDataBtn.addEventListener('click', function(){
-	// Cache input weight vector:
-	let w0 = document.getElementById("w0").value || 0;
-	let w1 = document.getElementById("w1").value || 0;
-	let w2 = document.getElementById("w2").value || 0;
-	let w3 = document.getElementById("w3").value || 0;
-	let w4 = document.getElementById("w4").value || 0;
-
-	let inputWeightVector = [w0, w1, w2, w3, w4];
-	console.clear();
-
-	// Classify new data
-	classify(E, inputWeightVector);
-});
-
-
-
-// Initial Weight vector corresponds to every feature
-// in our dataset:
-// 				x0 , x1, x2, x3, x4
+// Initial Weight vector corresponds to every feature in our dataset:
+// 		  x0 , x1, x2, x3, x4
 const W = [0,   0,  0,  0,  0];
-
-let breakEarly = false;
 let epochs = 5;
 
-// Saved models from previous data
-//Model from training iris-modified.setAB data (full set):
-// W2 = [0.2,-0.06666666680000002,0.2416666666,-0.23414634120000002,-0.22352941200000004];
 
-//Model from training iris-modified-setAB.csv data ( about 60 instances);
-// W3 = [0.2,-0.15555555580000002,0.1499999998,-0.2585365854,-0.27058823540000004];
+// Function definitions
+function handleTrainBtn(){
+	console.clear();
+	let trainedModel = trainModel(trainingData, epochs, W);
+	
+	trainedModelP.innerHTML = `W2 = [ ${trainedModel.join(", ")} ]`;
+}
+function handleTestBtn(){
+	// Cache user input for weight vector to classify data with:
+	const model = Array.from(document.querySelectorAll('.weight-vector'))
+				.map(w => w.value || 0);
 
-// Now Classify with new data.
-// Test data , E[], brought it from /js/data.js
+	console.clear();
+	// Classify new data
+	classify(testData, model);
+}
 
-let correctlyClassifiedRate = 0;
+
+// Event Listeners:
+trainModelBtn.addEventListener('click', handleTrainBtn);
+testDataBtn.addEventListener('click', handleTestBtn);
